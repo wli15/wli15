a0431bdc78e3c544b655f6b54f14a8807e29eda1
wli15 <61180367+wli15@users.noreply.github.com>
2020-10-01 18:39:29 -0400

    k07 completed ver with csv reader

--- 07/07.py
+++ 07/07.py
@@ -1,10 +1,23 @@
-from random import choice
-JobClass = {
-	
-}
-x=randint(0,99.8)
-for key, value in JobClass.items():
-	x=x-value
-	if x<0:
-		print(key)
-		break
+# Liam Kronman, William Li (Team WinLose)
+# SoftDev
+# K07 - Stl/O: Divine your Destiny (Occupation getter)
+# 2020-10-01
+import random, csv
+
+def jobGetter(dict):
+    x=random.uniform(0,float(dict["Total"]))
+    for key, value in dict.items():
+        x=x-float(value)
+        if x<=0:
+            return key
+    return "Invalid"
+
+if __name__ == "__main__":
+    JobClass = {}
+    with open("occupations.csv") as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            JobClass[row[0]] = row[1]
+    JobClass.pop("Job Class")
+    print(jobGetter(JobClass))
+
