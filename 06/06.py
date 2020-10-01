1e0c4ca55f626c57522b86410e6420be71db020c
wli15 <61180367+wli15@users.noreply.github.com>
2020-10-01 18:08:21 -0400

    added 06 hw assignment

---
+++ 06/06.py
@@ -0,0 +1,19 @@
+# Liam Kronman, William Li
+# SoftDev
+# K06 - Learnination Through Amalgamation (KREWES team selector updated)
+# 2020-10-01
+
+import random
+
+KREWES = {
+    'orpheus': ['ERIC', 'SAUVE', 'JONATHAN', 'PAK', 'LIAM', 'WINNIE', 'KELLY', 'JEFFREY', 'KARL', 'ISHITA', 'VICTORIA', 'BENJAMIN', 'ARIB', 'AMELIA', 'CONSTANCE', 'IAN'],
+    'rex': ['ANYA', 'DUB-Y', 'JESSICA', 'ALVIN', 'HELENA', 'MICHELLE', 'SHENKER', 'ARI', 'STELLA', 'RENEE', 'MADELYN', 'MAC', 'RYAN', 'DRAGOS'],
+    'endymion': ['JASON', 'DEAN', 'MADDY', 'SAQIF', 'CINDY', 'YI LING', 'RUOSHUI', 'FB', 'MATTHEW', 'MAY', 'ERIN', 'MEIRU']
+}
+
+team = input("Please enter a team you would like to target: ").lower()
+
+while team not in KREWES:
+	team = input("Please re-enter a valid team name: ").lower()
+
+print(random.choice(KREWES[team]))
