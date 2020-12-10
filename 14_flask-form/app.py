21177c1e04234068e5ecd0742e21ff84c966560b
wli15 <61180367+wli15@users.noreply.github.com>
2020-12-09 23:19:53 -0500

    k14

---
+++ 14_flask-form/app.py
@@ -0,0 +1,54 @@
+#Team MadCauseBad - Ryan Ma, Jessica Yeung, May Hathaway, William Li
+#SoftDev
+#K14 -- Form and Function - using post/get forms in flask
+#2020-12-10
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+
+@app.route("/", methods=['GET', 'POST'])
+def disp_loginpage():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    return render_template( 'login.html' )
+
+
+@app.route("/auth", methods=['GET', 'POST'])
+def authenticate():
+    print("\n\n\n")
+    print("***DIAG: this Flask obj ***")
+    print(app)
+    print("***DIAG: request obj ***")
+    print(request)
+    print("***DIAG: request.args ***")
+    print(request.args)
+    #print("***DIAG: request.args['username']  ***")
+    #print(request.args['username'])
+    print("***DIAG: request.headers ***")
+    print(request.headers)
+    req=request.form #Takes in values from dictionary from the form in login.html
+    print(req)
+    return render_template('response.html', usern=req['username'], meth=request.method)#response to a form submission
+
+
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
