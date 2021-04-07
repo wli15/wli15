46118dd340be1003d133d43e797cbe0961c1f094
wli15 <61180367+wli15@users.noreply.github.com>
2021-04-06 21:09:22 -0400

    k19 work added

---
+++ 19_rest/app.py
@@ -0,0 +1,25 @@
+#Team EmptyCup - William Li, Pak Lau
+#Soft Dev
+#K19 -- A RESTful Journey Skyward - using API keys to display image from another site
+#2021-04-06
+
+from flask import Flask, render_template
+import urllib.request as urllib2
+import json
+app = Flask(__name__)
+
+key = open("key_nasa.txt", "r")
+fullLink= "https://api.nasa.gov/planetary/apod?api_key=" + key.read()
+key.close()
+
+@app.route("/",  methods=["GET"])
+def root():
+	u = urllib2.urlopen(fullLink)
+	response = u.read()
+	data = json.loads(response)
+	return render_template("main.html", pic = data['url'])
+
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True
+    app.run()
